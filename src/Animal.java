41e7e5a1ce3c9b617fb0405f899fc4416c881d10
pasi <pasinduniwantha4@gmail.com>
2023-04-26 14:07:47 +0530

    Inheritance practical

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+class Dog extends Animal{
+    void bark() {
+        System.out.println("Baw Baw");
+    }
+}
+class TestInheritance{
+    public static void main(String[] args) {
+
+    }
+}
